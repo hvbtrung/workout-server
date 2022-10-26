40e1ac17486afdecc1194e7385c9d0d36c622b6f
baotrung <hvbtrung@gmail.com>
2022-10-27 01:13:15 +0700

    base cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
@@ -8,6 +9,8 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 // middleware
+app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
